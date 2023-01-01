6587ef363d4b3a15cc24200bde05dbb85b51575e
Al Amin <almin.cont@gmail.com>
2023-01-02 00:00:46 +0600

    s to S

--- components/layout/@asstes/NavBar.jsx
+++ components/layout/@asstes/NavBar.jsx
@@ -157,7 +157,7 @@ const NavBar = () => {
                                         href="#"
                                         className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0"
                                     >
-                                        Best seller
+                                        Best Seller
                                     </a>
                                 </li>
                                 <li>
